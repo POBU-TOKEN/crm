10157bfdf1b292b242a9d4542c04b674b6f5c7ed
Bernardo Issenguel <beissenguel12@gmail.com>
2025-06-26 14:44:25 +0100

    ensure users see only their activity contacts

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,21 @@
 from typing import Any
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+from django.db.models.query import QuerySet
 from account.models import User, Contact, Activity
+
+class ContactFilter(SimpleListFilter):
+    title = "Contact"
+    parameter_name = 'contact'
+
+    def lookups(self, request, model_admin):
+        contacts = Contact.objects.filter(user=request.user).values_list('id', 'name')
+        return contacts
+    
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(contact__id=self.value())
+        return queryset.filter(contact__user=request.user)
 
 # Register your models here.
 @admin.register(User)
@@ -13,7 +28,6 @@ class ContactAdmin(admin.ModelAdmin):
     fields = ('name', 'surname', 'phone', 'email', 'company')
     list_display = ('name', 'surname', 'phone', 'email', 'company', 'user__username')
     search_fields = ('name', )
-    list_filter = ('user',)
 
     def get_queryset(self, request):
         user = request.user
@@ -30,5 +44,5 @@ class ContactAdmin(admin.ModelAdmin):
 class ActivityAdmin(admin.ModelAdmin):
     fields = ('name', 'activity_type', 'description', 'contact')
     list_display = ('name', 'activity_type', 'description', 'contact')
-    list_filter = ('contact', 'activity_type')
+    list_filter = (ContactFilter, 'activity_type', )
     raw_id_fields = ('contact', )
