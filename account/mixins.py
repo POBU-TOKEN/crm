25e6157e64d20e244c5a4ded8f330e787f896a42
Bernardo Issenguel <beissenguel12@gmail.com>
2025-06-27 13:45:46 +0100

    simplify indentifier names

--- account/mixins.py
+++ account/mixins.py
@@ -10,10 +10,10 @@ class SeeLogButtonMixin:
 
         # Get content_type of current model
         model_content_type = ContentType.objects.get_for_model(self.model)
-        extra_context['current_model_content_type_id'] = model_content_type.id
-        extra_context['current_model_verbose_name'] = self.model._meta.verbose_name.capitalize()
+        extra_context['model_content_type_id'] = model_content_type.id
+        extra_context['model_verbose_name'] = self.model._meta.verbose_name.capitalize()
 
-        extra_context['current_model_app_label'] = self.model._meta.app_label
-        extra_context['current_model_model_name'] = self.model._meta.model_name
+        extra_context['model_app_label'] = self.model._meta.app_label
+        extra_context['model_model_name'] = self.model._meta.model_name
 
         return super().changelist_view(request, extra_context=extra_context)
